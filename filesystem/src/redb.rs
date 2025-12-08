6b6555efd8dbc0e6b524fc324ad20364ea8825f6
Lorenzo Tucci <yamabiiko@protonmail.com>
2025-12-08 05:04:28 +0100

    test(fs): increase waiting time in `save_load_db`

--- filesystem/src/redb.rs
+++ filesystem/src/redb.rs
@@ -239,7 +239,7 @@ mod tests {
         let shelves = fs.shelves.clone();
         let dirs = fs.dirs.clone();
         fs.close();
-        std::thread::sleep(Duration::from_millis(50));
+        std::thread::sleep(Duration::from_millis(100));
         drop(fs);
 
         let new_db_path = test_path.join("new_database.redb");
